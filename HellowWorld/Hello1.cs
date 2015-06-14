ae41726022e8a523ad6eaeda90e6e1fad64581cb
paltakis <paltakis@yahoo.com>
2015-06-14 01:18:57 +0000

    My first C# program

---
+++ HellowWorld/Hello1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorld
+{
+    public class Hello1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
